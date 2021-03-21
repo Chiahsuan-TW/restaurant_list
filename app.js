40d5bb7af749f6754a703dc01e56347bb1940ac9
Chiahsuan Chou <belinda05021992@gmail.com>
2021-03-21 11:38:54 +0800

    feat: connect to MongoDB and add restaurant seeder

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/restaurant_list', {useNewUrlParser: true, useUnifiedTopology: true})
+
+
+const db = mongoose.connection
+
+db.on('error', ()=>{
+    console.log('mongodb error!')
+})
+
+db.once('open', ()=>{
+    console.log('mongodb connected!')
+})
 
 //setting template engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
